99ef2221a5414f020b9aa25a27bba166b13e26c0
thavao <othavioact@gmail.com>
2024-04-21 19:16:04 -0300

    inserindo diferenciacao por sexo

--- CalculoIMC/Program.cs
+++ CalculoIMC/Program.cs
@@ -1,4 +1,5 @@
 ﻿double altura, peso, imc;
+int sexo;
 
 
 Console.Write("Digite o peso em Kg: ");
@@ -7,26 +8,53 @@ peso = double.Parse(Console.ReadLine());
 Console.Write("Digite a altura em metro: ");
 altura = double.Parse(Console.ReadLine());
 
+Console.WriteLine("Selecione o sexo: ");
+Console.WriteLine("[1] - feminino | [2] - masculino");
+sexo = int.Parse(Console.ReadLine());
+
 imc = peso / (altura * altura);
 
-Console.WriteLine("");
-if (imc < 18.5)
-    Console.WriteLine("Baixo peso");
+Console.WriteLine("Resultado do IMC: ");
 
-else if (imc >= 18.5 && imc <= 24.9)
-    Console.WriteLine("Peso normal");
+if (sexo == 1)
+{
+    if (imc < 19.1)
+        Console.WriteLine("Abaixo do peso");
 
-else if (imc >= 25 && imc <= 29.9)
-    Console.WriteLine("Sobrepeso");
+    else if (imc >= 19.1 && imc <= 25.8)
+        Console.WriteLine("Peso ideal");
 
-else if (imc >= 30 && imc <= 34.9)
-    Console.WriteLine("Obesidade grau I");
+    else if (imc >= 25.9 && imc <= 27.3)
+        Console.WriteLine("Pouco acima do peso");
 
-else if (imc >= 35 && imc <= 39.9)
-    Console.WriteLine("Obesidade grau II");
+    else if (imc >= 27.4 && imc <= 32.3)
+        Console.WriteLine("Acima do peso");
 
+    else
+        Console.WriteLine("Obesidade");
+}
+else if (sexo == 2)
+{
+    if (imc < 20.7)
+        Console.WriteLine("Abaixo do peso");
+
+    else if (imc >= 20.7 && imc <= 26.4)
+        Console.WriteLine("Peso ideal");
+
+    else if (imc >= 26.5 && imc <= 27.8)
+        Console.WriteLine("Pouco acima do peso");
+
+    else if (imc >= 27.9 && imc <= 31.1)
+        Console.WriteLine("Acima do peso");
+
+    else
+        Console.WriteLine("Obesidade");
+}
 else
-    Console.WriteLine("Obesidade grau III");
+{
+    Console.WriteLine("Sexo inválido...");
+}
+
 
 
 Console.WriteLine("Pressione Enter para continuar...");
